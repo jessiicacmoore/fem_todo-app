ce24c16a5341109e106c208a897a97a6875d5676
Jessica Moore <jessica.cmoore1@gmail.com>
2024-12-10 16:57:06 -0500

    :sparkles: Add ability to save todos in local storage

--- src/context/todos/todoContext.tsx
+++ src/context/todos/todoContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, ReactNode } from "react";
+import React, { createContext, useReducer, useEffect, ReactNode } from "react";
 import { todoReducer, Todo, TodoAction, TodoState } from "./todoReducer";
 import { defaultTodos } from "../../constants/defaultTodos";
 
@@ -12,13 +12,22 @@ export const TodoContext = createContext<TodoContextType | undefined>(
   undefined,
 );
 
+function getInitialTodos(): Todo[] {
+  const savedTodos = localStorage.getItem("todos");
+  return savedTodos ? JSON.parse(savedTodos) : defaultTodos;
+}
+
 const initialState: TodoState = {
-  todos: defaultTodos,
+  todos: getInitialTodos(),
   filter: "all",
 };
 
 export function TodoProvider({ children }: { children: ReactNode }) {
   const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(state.todos));
+  }, [state.todos]);
 
   return (
     <TodoContext.Provider
